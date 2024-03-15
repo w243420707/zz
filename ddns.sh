e213078e2d3d07fa0da38a2f9413b757814713d4
www <yooyu@msn.com>
2024-03-16 00:19:19 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -37,7 +37,7 @@ CFUSER=yooyu@msn.com
 CFZONE_NAME=fly64jfgwhale.xyz
 
 # Hostname to update, eg: homeserver.example.com
-CFRECORD_NAME=xn--fiqq40n.fly64jfgwhale.xyz
+CFRECORD_NAME=xk7ps2c7je71.fly64jfgwhale.xyz
 
 # Record type, A(IPv4)|AAAA(IPv6), default IPv4
 CFRECORD_TYPE=A
